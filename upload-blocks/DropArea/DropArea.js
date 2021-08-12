ebcdbf4e2b0c45af0f47cad3f7712020db7273de
Alex Sova <foxeyes.ml@gmail.com>
2021-08-12 18:58:08 +0300

    chore: drop-area drop

--- upload-blocks/DropArea/DropArea.js
+++ upload-blocks/DropArea/DropArea.js
@@ -1,6 +1,34 @@
 import { BlockComponent } from '../BlockComponent/BlockComponent.js';
 
-export class DropArea extends BlockComponent {}
+export class DropArea extends BlockComponent {
+
+  initCallback() {
+    this.addEventListener('dragover', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+    });
+    this.addEventListener('drop', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      if (e.dataTransfer.files) {
+        [...e.dataTransfer.files].forEach((/** @type {File} */ file) => {
+          this.uploadCollection.add({
+            file,
+            isImage: file.type.includes('image'),
+            mimeType: file.type,
+            fileName: file.name,
+            fileSize: file.size,
+          });
+        });
+        this.multiPub('external', {
+          currentActivity: 'upload-list',
+          modalActive: true,
+        });
+      }
+    }, false);
+  }
+
+}
 
 DropArea.template = /*html*/ `
 <div .sources>
