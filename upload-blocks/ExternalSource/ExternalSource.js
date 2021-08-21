2662cd7099bab0cafe539001a37fd0daa18b91c1
nd0ut <nd0ut.me@gmail.com>
2021-08-21 15:29:53 +0300

    feat: external frame styling

--- upload-blocks/ExternalSource/ExternalSource.js
+++ upload-blocks/ExternalSource/ExternalSource.js
@@ -2,19 +2,34 @@ import { BlockComponent } from '../BlockComponent/BlockComponent.js'
 import { registerMessage, unregisterMessage } from './messages.js'
 import { uploadFromUrl, getInfo } from '../../common-utils/UploadClientLight.js'
 
+let styleToCss = (style) => {
+  let css = Object.keys(style).reduce((acc, selector) => {
+    let propertiesObj = style[selector]
+    let propertiesStr = Object.keys(propertiesObj).reduce((acc, prop) => {
+      let value = propertiesObj[prop]
+      return acc + `${prop}: ${value};`
+    }, '')
+    return acc + `${selector}{${propertiesStr}}`
+  }, '')
+  return css
+}
+
 export class ExternalSource extends BlockComponent {
   constructor() {
     super()
 
     this._externalSourceType = null
     this._iframe = null
+
+    this.initLocalState({
+      counter: 0,
+      'on.back': () => {
+        this.pub('external', 'currentActivity', 'upload-list')
+      },
+    })
   }
 
   initCallback() {
-    this.style.display = 'flex'
-    this.style.position = 'relative'
-    this.style.backgroundColor = '#ffffff'
-
     this.addToExternalState({
       externalSourceType: null,
     })
@@ -25,6 +40,7 @@ export class ExternalSource extends BlockComponent {
 
     this.externalState.sub('currentActivity', (val) => {
       if (val === 'external') {
+        this.pub('local', 'counter', 0)
         this.mountIframe()
       } else if (this._iframe) {
         this.unmountIframe()
@@ -32,14 +48,19 @@ export class ExternalSource extends BlockComponent {
     })
   }
 
+  sendMessage(message) {
+    this._iframe.contentWindow.postMessage(JSON.stringify(message), '*')
+  }
+
   async handleFileSelected(message) {
+    this.pub('local', 'counter', this.localState.read('counter') + 1)
+
     // TODO: check for alternatives, see https://github.com/uploadcare/uploadcare-widget/blob/f5d3e8c9f67781bed2eb69814c8f86a4cc035473/src/widget/tabs/remote-tab.js#L102
     let { url } = message
     let pubkey = this.config.PUBKEY
     let entry = this.uploadCollection.add({
       externalUrl: url,
     })
-    this.pub('external', 'currentActivity', 'upload-list')
     await uploadFromUrl(url, pubkey, async (info) => {
       if (info.type === 'progress') {
         entry.setValue('uploadProgress', info.progress)
@@ -58,16 +79,48 @@ export class ExternalSource extends BlockComponent {
     })
   }
 
+  handleIframeLoad(e) {
+    this.applyStyles()
+  }
+
+  getCssValue(propName) {
+    let style = window.getComputedStyle(this)
+    return style.getPropertyValue(propName).trim()
+  }
+
+  applyStyles() {
+    let styleObj = {
+      body: {
+        color: this.getCssValue('--clr-txt'),
+      },
+      '.side-bar': {
+        'background-color': this.getCssValue('--clr-shade-1'),
+      },
+      '.list-table-row': {
+        color: this.getCssValue('--clr-txt'),
+      },
+      '.list-table-row:hover': {
+        background: this.getCssValue('--clr-shade-1'),
+      },
+    }
+
+    this.sendMessage({
+      type: 'embed-css',
+      style: styleToCss(styleObj),
+    })
+  }
+
   remoteUrl() {
     let pubkey = this.config.PUBKEY
     let version = '3.11.3'
-    let imagesOnly = (false).toString()
+    let imagesOnly = false.toString()
 
     return `https://social.uploadcare.com/window3/${this._externalSourceType}?lang=en&public_key=${pubkey}&widget_version=${version}&images_only=${imagesOnly}&pass_window_open=false`
   }
 
   mountIframe() {
     let iframe = document.createElement('iframe')
+    iframe.addEventListener('load', this.handleIframeLoad.bind(this))
 
     iframe.setAttribute('src', this.remoteUrl())
     iframe.setAttribute('marginheight', '0')
@@ -75,13 +128,9 @@ export class ExternalSource extends BlockComponent {
     iframe.setAttribute('frameborder', '0')
     iframe.setAttribute('allowTransparency', 'true')
 
-    iframe.style.width = '100%'
-    iframe.style.height = '100%'
-
-    this.innerHTML = ''
-    this.style.width = '600px'
-    this.style.height = '400px'
-    this.appendChild(iframe)
+    let wrapper = this.ref['iframe-wrapper']
+    wrapper.innerHTML = ''
+    wrapper.appendChild(iframe)
 
     registerMessage(
       'file-selected',
@@ -95,11 +144,22 @@ export class ExternalSource extends BlockComponent {
   unmountIframe() {
     unregisterMessage('file-selected', this._iframe.contentWindow)
 
-    this.style.width = '0px'
-    this.style.height = '0px'
-    this.innerHTML = ''
+    let wrapper = this.ref['iframe-wrapper']
+    wrapper.innerHTML = ''
     this._iframe = undefined
   }
 }
 
-ExternalSource.template = /*html*/ ``
+ExternalSource.template = /*html*/ `
+<div ref="iframe-wrapper" .iframe-wrapper>
+</div>
+<div .toolbar>
+  <button .back-btn loc="onclick: on.back">
+    <icon-ui name="back"></icon-ui>
+  </button>
+  <div .selected-counter>
+    <span l10n="selected-count"></span>
+    <span loc="textContent: counter"></span>
+  </div>
+</div>
+`
