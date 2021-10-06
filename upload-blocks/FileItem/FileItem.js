9748008ff3efe9f590953c3816115357a55fdefb
Alex Sova <foxeyes.ml@gmail.com>
2021-10-06 08:57:52 +0300

    wip: blocks ui

--- upload-blocks/FileItem/FileItem.js
+++ upload-blocks/FileItem/FileItem.js
@@ -7,6 +7,9 @@ export class FileItem extends BlockComponent {
   init$ = {
     fileName: '',
     thumb: '',
+    thumbUrl: '',
+    progressWidth: 0,
+    progressOpacity: 1,
     notImage: true,
     badgeIcon: 'check',
     onEdit: () => {
@@ -24,12 +27,6 @@ export class FileItem extends BlockComponent {
     /** @type {import('../../symbiote/core/TypedData.js').TypedData} */
     this.entry = this.uploadCollection?.read(id);
 
-    this.entry.subscribe('uuid', (uuid) => {
-      if (uuid) {
-        this.setAttribute('loaded', '');
-      }
-    });
-
     this.entry.subscribe('fileName', (name) => {
       this.$.fileName = name || 'No name...';
     });
@@ -38,22 +35,23 @@ export class FileItem extends BlockComponent {
       if (!uuid) {
         return;
       }
+      this.setAttribute('loaded', '');
       let url = `https://ucarecdn.com/${uuid}/`;
-      this.ref.thumb.style.backgroundImage = `url(${url}-/scale_crop/76x76/)`;
+      this.$.thumbUrl = `url(${url}-/scale_crop/76x76/)`;
     });
 
     this.entry.subscribe('transformationsUrl', (transformationsUrl) => {
       if (!transformationsUrl) {
         return;
       }
-      this.ref.thumb.style.backgroundImage = `url(${transformationsUrl}-/scale_crop/76x76/)`;
+      this.$.thumbUrl = `url(${transformationsUrl}-/scale_crop/76x76/)`;
     });
 
     this.file = this.entry.getValue('file');
 
     if (this.file?.type.includes('image')) {
       resizeImage(this.file, 76).then((img) => {
-        this.ref.thumb.style.backgroundImage = `url(${img})`;
+        this.$.thumbUrl = `url(${img})`;
       });
     }
   }
@@ -84,7 +82,6 @@ export class FileItem extends BlockComponent {
   }
 
   destroyCallback() {
-    super.disconnectedCallback();
     FileItem.activeInstances.delete(this);
   }
 
@@ -92,7 +89,7 @@ export class FileItem extends BlockComponent {
     if (this.hasAttribute('loaded') || this.entry.getValue('uuid')) {
       return;
     }
-    this.ref.progress.style.width = '0';
+    this.$.progressWidth = 0;
     this.removeAttribute('focused');
     this.removeAttribute('error');
     this.setAttribute('uploading', '');
@@ -102,16 +99,18 @@ export class FileItem extends BlockComponent {
         publicKey: this.config.PUBKEY,
         onProgress: (progress) => {
           let percentage = progress.value * 100;
-          this.ref.progress.style.width = percentage + '%';
+          this.$.progressWidth = percentage + '%';
           this.entry.setValue('uploadProgress', percentage);
         },
       });
-      this.ref.progress.style.opacity = '0';
+      this.$.progressOpacity = 0;
       this.setAttribute('loaded', '');
       this.removeAttribute('uploading');
       this.$.badgeIcon = 'check';
-      this.entry.setValue('uuid', fileInfo.uuid);
-      this.entry.setValue('uploadProgress', 100);
+      this.entry.setMultipleValues({
+        uuid: fileInfo.uuid,
+        uploadProgress: 100,
+      });
     } catch (error) {
       this.setAttribute('error', '');
       this.removeAttribute('uploading');
@@ -129,15 +128,21 @@ export class FileItem extends BlockComponent {
 }
 
 FileItem.template = /*html*/ `
-<div .thumb ref="thumb"></div>
-<div file-name set="textContent: fileName"></div>
+<div 
+  .thumb 
+  set="style.backgroundImage: thumbUrl">
+</div>
+<div .file-name set="textContent: fileName"></div>
 <div .badge>
   <uc-icon set="@name: badgeIcon"></uc-icon>
 </div>
 <button .edit-btn set="onclick: onEdit;">
   <uc-icon name="edit-file"></uc-icon>
 </button>
-<div ref="progress" .progress></div>
+<div 
+  .progress
+  set="style.width: progressWidth; style.opacity: progressOpacity">
+</div>
 `;
 FileItem.activeInstances = new Set();
 
