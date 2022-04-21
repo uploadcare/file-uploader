4d33c6bb0e9752637eed7bf7780dc4b5460f5e95
nd0ut <nd0ut.me@gmail.com>
2022-04-21 18:10:57 +0300

    chore: add todo

--- solutions/cloud-image-editor/index.js
+++ solutions/cloud-image-editor/index.js
@@ -1,4 +1,10 @@
 import * as CloudEditorBlocks from './CloudEditor.js';
+
+/* TODO: We need to make some dependency injection/checking magic
+I see it as a declared list of tags on which the block depends
+Then we can check whether the dependent tag is registered in the CustomElementRegistry or not.
+If not, register it from default ones or just log the warning */
+
 import { Icon } from '../../blocks/Icon/Icon.js';
 import { registerBlocks } from '../../abstract/registerBlocks.js';
 
