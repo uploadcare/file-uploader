16566f3542eb55ec8e7012d7003369a99c14f1bb
Alex Sova <foxeyes.ml@gmail.com>
2022-04-26 11:43:38 +0300

    chore: drop area hidden in case of single source (not local files)

--- solutions/file-uploader/inline/FileUploaderInline.js
+++ solutions/file-uploader/inline/FileUploaderInline.js
@@ -1,19 +1,26 @@
 import { Block } from '../../../abstract/Block.js';
 
 export class FileUploaderInline extends Block {
+  init$ = {
+    dropAreaHidden: false,
+  };
+
   initCallback() {
     this.sub('*currentActivity', (val) => {
       if (!val) {
         this.$['*currentActivity'] = this.initActivity || Block.activities.START_FROM;
       }
     });
+    if (this.sourceList.length === 1 && !this.sourceList.includes(Block.sourceTypes.LOCAL)) {
+      this.$.dropAreaHidden = true;
+    }
   }
 }
 
 FileUploaderInline.template = /*html*/ `
 <uc-start-from>
   <uc-source-list wrap></uc-source-list>
-  <uc-drop-area></uc-drop-area>
+  <uc-drop-area set="@hidden: dropAreaHidden"></uc-drop-area>
 </uc-start-from>
 <uc-upload-list></uc-upload-list>
 <uc-camera-source></uc-camera-source>
