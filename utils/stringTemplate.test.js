5be4d494ee7b5fc9523f53e74e8853fca3ab0dc3
nd0ut <nd0ut.me@gmail.com>
2022-05-04 15:59:50 +0300

    chore: setup unit test flow

--- utils/stringTemplate.test.js
+++ utils/stringTemplate.test.js
@@ -5,4 +5,20 @@ describe('stringTemplate', () => {
   it('should return the same string if no variables are passed', () => {
     expect(stringTemplate('Hello world!')).to.equal('Hello world!');
   });
+
+  it('should replace variables', () => {
+    expect(
+      stringTemplate("Hello world! My name is {{name}}. I'm {{age}} years old.", { name: 'John Doe', age: 12 })
+    ).to.equal("Hello world! My name is John Doe. I'm 12 years old.");
+  });
+
+  it('should work with variables at start/end', () => {
+    expect(stringTemplate("{{name}} my name is. I'm {{age}}", { name: 'John Doe', age: 12 })).to.equal(
+      "John Doe my name is. I'm 12"
+    );
+  });
+
+  it('should work with single variable', () => {
+    expect(stringTemplate('{{name}}', { name: 'John Doe' })).to.equal('John Doe');
+  });
 });
