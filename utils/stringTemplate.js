392dd7e424a554465244cba1c358669e0aed8482
nd0ut <nd0ut.me@gmail.com>
2022-05-05 14:15:07 +0300

    chore: improve `stringTemplate` performance

--- utils/stringTemplate.js
+++ utils/stringTemplate.js
@@ -1,19 +1,29 @@
+const KEY_NOT_FOUND = '__KEY_NOT_FOUND__';
+
 /**
  * @param {String} str
  * @param {{ [key: String]: String | Number }} [variables={}] Default is `{}`
- * @returns {String}
+ * @returns {{ string: String; missingKeys: String[] }}
  */
 export function stringTemplate(str, variables = {}) {
-  return str
-    .split('{{')
-    .map((part) => part.split('}}'))
-    .reduce((result, token) => {
-      if (token.length == 1) {
-        result += token[0];
-      } else {
-        result += variables[token[0]]?.toString();
-        result += token[1];
-      }
-      return result;
-    }, '');
+  let missingKeys = [];
+  let result = '';
+
+  for (let part of str.split('{{')) {
+    let tokens = part.split('}}');
+    if (tokens.length == 1) {
+      let text = tokens[0];
+      result += text;
+    } else {
+      let [key, text] = tokens;
+      let hasValue = variables[key] !== undefined;
+      let value = hasValue ? variables[key].toString() : KEY_NOT_FOUND;
+      !hasValue && missingKeys.push(key);
+
+      result += value;
+      result += text;
+    }
+  }
+
+  return { string: result, missingKeys: missingKeys };
 }
