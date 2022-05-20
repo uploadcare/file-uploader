f6c0e3941f125ae6f0f6abaf9ad741c8e375871d
nd0ut <nd0ut.me@gmail.com>
2022-05-20 14:29:28 +0300

    chore: add secure delivery proxy test server

---
+++ secure-delivery-proxy.js
@@ -0,0 +1,24 @@
+import http from 'http';
+
+const PORT = 3000;
+
+http
+  .createServer(function (request, response) {
+    if (request.method !== 'GET') {
+      return response.end('Only GET requests are supported');
+    }
+    let url = new URL(request.url, `http://localhost:${PORT}`);
+    let path = url.pathname.replace(/([^\/])$/, '$1/');
+    if (path !== '/preview/') {
+      return response.end('Only `/preview/` path is supported');
+    }
+    let searchParams = url.searchParams;
+    let fileUrl = searchParams.get('url');
+    if (!fileUrl) {
+      return response.end('`url` parameter is required');
+    }
+    response.statusCode = 302;
+    response.setHeader('Location', fileUrl);
+    response.end();
+  })
+  .listen(PORT);
