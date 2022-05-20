3e4e11436835a973a7816723b719ac1fc8936af6
nd0ut <nd0ut.me@gmail.com>
2022-05-20 19:32:34 +0300

    chore(applyTemplateData): add support for deep properties

--- utils/applyTemplateData.js
+++ utils/applyTemplateData.js
@@ -1,13 +1,45 @@
+/** @typedef {{ [key: String]: String | Number | Boolean | InputData }} InputData */
+
+import { getProperty } from './getProperty.js';
+
+const DEFAULT_TRANSFORMER = (value) => value;
+
+/**
+ * @typedef {Object} Options
+ * @property {String} [openToken='{{'] Default is `'{{'`
+ * @property {String} [closeToken='}}'] Default is `'}}'`
+ * @property {(value: String) => String} [transform=DEFAULT_TRANSFORMER] Default is `DEFAULT_TRANSFORMER`
+ */
+
 /**
  * @param {String} template
- * @param {{ [key: String]: String | Number }} [data={}] Default is `{}`
- * @param {String} [openToken='{{'] Default is `'{{'`
- * @param {String} [closeToken='}}'] Default is `'}}'`
+ * @param {InputData} [data={}] Default is `{}`
+ * @param {Options} [options={}] Default is `{}`
  * @returns {String}
  */
-export function applyTemplateData(template, data, openToken = '{{', closeToken = '}}') {
-  for (let key in data) {
-    template = template.replaceAll(openToken + key + closeToken, data[key]?.toString());
+export function applyTemplateData(
+  template,
+  data,
+  { openToken = '{{', closeToken = '}}', transform = DEFAULT_TRANSFORMER } = {}
+) {
+  let result = '';
+
+  for (let part of template.split(openToken)) {
+    let tokens = part.split(closeToken);
+    if (tokens.length == 1) {
+      let text = tokens[0];
+      result += text;
+    } else {
+      let [path, text] = tokens;
+      let value = getProperty(data, path)?.toString();
+      if (typeof value === 'string') {
+        result += transform(value);
+      } else {
+        result += openToken + path + closeToken;
+      }
+      result += text;
+    }
   }
-  return template;
+
+  return result;
 }
