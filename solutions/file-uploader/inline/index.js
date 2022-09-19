bc3ca228af0eedccca293545e7a720641f918627
nd0ut <nd0ut.me@gmail.com>
2022-09-19 18:29:45 +0300

    chore: remove unused code

--- solutions/file-uploader/inline/index.js
+++ solutions/file-uploader/inline/index.js
@@ -1,6 +1,2 @@
-import { FileUploaderInline } from './FileUploaderInline.js';
 import * as LR from '../../../index.js';
 LR.registerBlocks(LR);
-
-class Uploader extends FileUploaderInline {}
-Uploader.reg('uploader');
--- solutions/file-uploader/regular/index.js
+++ solutions/file-uploader/regular/index.js
@@ -1,6 +1,3 @@
 import * as LR from '../../../index.js';
 
 LR.registerBlocks(LR);
-
-class Uploader extends LR.FileUploaderRegular {}
-Uploader.reg('uploader');
