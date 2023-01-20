31a2c35bec73bd072ff3e73eb377015316cde581
Anton Nesterov <anesterov@uploadcare.com>
2023-01-20 13:03:10 +0100

    added credits

--- solutions/file-uploader/regular/FileUploaderRegular.js
+++ solutions/file-uploader/regular/FileUploaderRegular.js
@@ -9,6 +9,7 @@ FileUploaderRegular.template = /* HTML */ `
     <lr-start-from>
       <lr-drop-area with-icon clickable></lr-drop-area>
       <lr-source-list wrap></lr-source-list>
+      <a href="https://uploadcare.com/" target="_blank" class="credits">Powered by Uploadcare</a>
     </lr-start-from>
     <lr-upload-list></lr-upload-list>
     <lr-camera-source></lr-camera-source>
