072e4984cfd16df21a4aa51a97a780484f755224
nd0ut <nd0ut.me@gmail.com>
2023-01-24 21:43:32 +0400

    chore: allow to pass children to the solution block wrappers

--- abstract/SolutionBlock.js
+++ abstract/SolutionBlock.js
@@ -4,4 +4,13 @@ import { uploaderBlockCtx } from './CTX.js';
 export class SolutionBlock extends ShadowWrapper {
   ctxInit = uploaderBlockCtx(this);
   ctxOwner = true;
+  _template = null;
+
+  static set template(value) {
+    this._template = value + /** HTML */ `<slot></slot>`;
+  }
+
+  static get template() {
+    return this._template;
+  }
 }
