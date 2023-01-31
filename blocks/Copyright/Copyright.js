90f57b7b07e4d88c0576561105a511f9cb420b3d
nd0ut <nd0ut.me@gmail.com>
2023-01-31 12:04:10 +0400

    chore: update copyright url

--- blocks/Copyright/Copyright.js
+++ blocks/Copyright/Copyright.js
@@ -2,6 +2,11 @@ import { Block } from '../../abstract/Block.js';
 
 export class Copyright extends Block {
   static template = /* HTML */ `
-    <a href="https://uploadcare.com/" target="_blank" class="credits">Powered by Uploadcare</a>
+    <a
+      href="https://uploadcare.com/?utm_source=copyright&utm_medium=referral&utm_campaign=v4"
+      target="_blank noopener"
+      class="credits"
+      >Powered by Uploadcare</a
+    >
   `;
 }
