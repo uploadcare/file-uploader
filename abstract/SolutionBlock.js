3475a2c92ebfe1f634035621ed08a7e3d3eaaf54
nd0ut <nd0ut.me@gmail.com>
2023-02-27 22:31:21 +0400

    fix(types): allow solution template overriding

--- abstract/SolutionBlock.js
+++ abstract/SolutionBlock.js
@@ -4,12 +4,25 @@ import { uploaderBlockCtx } from './CTX.js';
 export class SolutionBlock extends ShadowWrapper {
   ctxInit = uploaderBlockCtx(this);
   ctxOwner = true;
-  _template = null;
 
+  /**
+   * @private
+   * @type {String}
+   */
+  static _template = null;
+
+  /**
+   * @param {String} value
+   * @public
+   */
   static set template(value) {
     this._template = value + /** HTML */ `<slot></slot>`;
   }
 
+  /**
+   * @returns {String}
+   * @public
+   */
   static get template() {
     return this._template;
   }
