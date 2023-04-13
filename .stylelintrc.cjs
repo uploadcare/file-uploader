a810baaab42c34986e1918a691725957d043a83b
Aleksandr Grenishin <nd0ut.me@gmail.com>
2023-04-13 16:10:58 +0300

    chore: bump dependencies (#416)
    * chore: bump dev dependencies
    
    * chore: replace `stylelint-config-rational-order` with `stylelint-order`
    
    * chore: disable new stylelint rule
    
    * chore: format files

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['stylelint-config-rational-order', 'stylelint-config-standard', 'stylelint-config-prettier'],
-  plugins: ['stylelint-declaration-block-no-ignored-properties'],
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
   rules: {
     'plugin/declaration-block-no-ignored-properties': true,
     'function-calc-no-unspaced-operator': true, // can cause out of memory in some cases
@@ -18,7 +18,8 @@ module.exports = {
       },
     ],
     'color-function-notation': null,
-    'order/properties-order': null,
+    'order/order': ['custom-properties', 'declarations'],
+    'order/properties-order': ['width', 'height'],
     'rule-empty-line-before': null,
     'at-rule-no-unknown': [
       true,
@@ -32,6 +33,7 @@ module.exports = {
         ignoreProperties: ['container-type', 'container-name'],
       },
     ],
+    'media-feature-range-notation': null,
   },
   overrides: [
     {
