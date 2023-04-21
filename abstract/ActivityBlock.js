c2eb303abf099b328e7aed670b080ca17bb5d758
nd0ut <nd0ut.me@gmail.com>
2023-04-21 19:09:05 +0300

    chore(activity-block): refactor code

--- abstract/ActivityBlock.js
+++ abstract/ActivityBlock.js
@@ -12,6 +12,28 @@ export class ActivityBlock extends Block {
 
   _debouncedHistoryFlush = debounce(this._historyFlush.bind(this), 10);
 
+  /** @private */
+  _deactivate() {
+    let actDesc = ActivityBlock._activityRegistry[this.activityKey];
+    this[ACTIVE_PROP] = false;
+    this.removeAttribute(ACTIVE_ATTR);
+    actDesc?.deactivateCallback?.();
+    // console.log(`Activity "${this.activityType}" deactivated`);
+  }
+
+  /** @private */
+  _activate() {
+    let actDesc = ActivityBlock._activityRegistry[this.activityKey];
+    this.$['*historyBack'] = this.historyBack.bind(this);
+    /** @private */
+    this[ACTIVE_PROP] = true;
+    this.setAttribute(ACTIVE_ATTR, '');
+    actDesc?.activateCallback?.();
+    // console.log(`Activity "${this.activityType}" activated`);
+
+    this._debouncedHistoryFlush();
+  }
+
   initCallback() {
     super.initCallback();
     if (this.hasAttribute('current-activity')) {
@@ -26,23 +48,10 @@ export class ActivityBlock extends Block {
         this.setAttribute('activity', this.activityType);
       }
       this.sub('*currentActivity', (/** @type {String} */ val) => {
-        let actDesc = ActivityBlock._activityRegistry[this.activityKey];
-
         if (this.activityType !== val && this[ACTIVE_PROP]) {
-          /** @private */
-          this[ACTIVE_PROP] = false;
-          this.removeAttribute(ACTIVE_ATTR);
-          actDesc?.deactivateCallback?.();
-          // console.log(`Activity "${this.activityType}" deactivated`);
+          this._deactivate();
         } else if (this.activityType === val && !this[ACTIVE_PROP]) {
-          this.$['*historyBack'] = this.historyBack.bind(this);
-          /** @private */
-          this[ACTIVE_PROP] = true;
-          this.setAttribute(ACTIVE_ATTR, '');
-          actDesc?.activateCallback?.();
-          // console.log(`Activity "${this.activityType}" activated`);
-
-          this._debouncedHistoryFlush();
+          this._activate();
         }
 
         if (!val) {
@@ -64,6 +73,11 @@ export class ActivityBlock extends Block {
       }
       this.$['*history'] = history;
     }
+  }
+
+  /** @private */
+  _isActivityRegistered() {
+    return this.activityType && !!ActivityBlock._activityRegistry[this.activityKey];
   }
 
   /**
@@ -97,14 +111,14 @@ export class ActivityBlock extends Block {
 
   unregisterActivity() {
     if (this.isActivityActive) {
-      ActivityBlock._activityRegistry[this.activityKey]?.deactivateCallback?.();
+      this._deactivate();
     }
-    delete ActivityBlock._activityRegistry[this.activityKey];
+    ActivityBlock._activityRegistry[this.activityKey] = undefined;
   }
 
   destroyCallback() {
     super.destroyCallback();
-    this.unregisterActivity();
+    this._isActivityRegistered() && this.unregisterActivity();
 
     if (Object.keys(ActivityBlock._activityRegistry).length === 0) {
       // TODO: we should track activities more precise and reset current activity only if there is no such registered activity
