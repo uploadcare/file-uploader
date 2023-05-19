3b933dce26f888af6a87c595fd8f38947fe2c1a9
nd0ut <nd0ut.me@gmail.com>
2023-05-19 14:51:52 +0300

    chore: remove docs

--- blocks/DropArea/test.js
+++
@@ -1,7 +0,0 @@
-import { ifRef } from '../../utils/ifRef.js';
-import { DropArea } from './DropArea.js';
-import { registerBlocks } from '../../abstract/registerBlocks.js';
-
-ifRef(() => {
-  registerBlocks({ DropArea });
-});
