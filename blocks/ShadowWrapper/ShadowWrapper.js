4b73b9fb4ecc3fbcc5f6bbe152ab7b118a4d3884
nd0ut <nd0ut.me@gmail.com>
2023-06-29 12:34:34 +0300

    chore: wait for css-src async

--- blocks/ShadowWrapper/ShadowWrapper.js
+++ blocks/ShadowWrapper/ShadowWrapper.js
@@ -11,33 +11,37 @@ export class ShadowWrapper extends Block {
   initCallback() {
     super.initCallback();
     this.setAttribute('hidden', '');
-    let href = this.getAttribute(CSS_ATTRIBUTE);
-    if (href) {
-      this.renderShadow = true;
-      this.attachShadow({
-        mode: 'open',
-      });
-      let link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.type = 'text/css';
-      link.href = href;
-      link.onload = () => {
-        // CSS modules can be not loaded at this moment
-        // TODO: investigate better solution
-        window.requestAnimationFrame(() => {
-          this.render();
-          window.setTimeout(() => {
-            this.removeAttribute('hidden');
-            this.shadowReadyCallback();
-          });
+
+    // wait for attribute async
+    setTimeout(() => {
+      let href = this.getAttribute(CSS_ATTRIBUTE);
+      if (href) {
+        this.renderShadow = true;
+        this.attachShadow({
+          mode: 'open',
         });
-      };
-      // @ts-ignore TODO: fix this
-      this.shadowRoot.prepend(link);
-    } else {
-      this.render();
-      this.removeAttribute('hidden');
-      this.shadowReadyCallback();
-    }
+        let link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = href;
+        link.onload = () => {
+          // CSS modules can be not loaded at this moment
+          // TODO: investigate better solution
+          window.requestAnimationFrame(() => {
+            this.render();
+            window.setTimeout(() => {
+              this.removeAttribute('hidden');
+              this.shadowReadyCallback();
+            });
+          });
+        };
+        // @ts-ignore TODO: fix this
+        this.shadowRoot.prepend(link);
+      } else {
+        this.render();
+        this.removeAttribute('hidden');
+        this.shadowReadyCallback();
+      }
+    }, 0);
   }
 }
