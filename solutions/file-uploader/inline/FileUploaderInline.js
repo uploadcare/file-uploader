44bd8452a1f720d2c21444f64b5a74c0ffe26880
Aleksandr Grenishin <nd0ut.me@gmail.com>
2023-11-09 17:08:44 +0300

    feat: add `cancel` button on the start from activity (#554)

--- solutions/file-uploader/inline/FileUploaderInline.js
+++ solutions/file-uploader/inline/FileUploaderInline.js
@@ -1,7 +1,16 @@
-import { SolutionBlock } from '../../../abstract/SolutionBlock.js';
+// @ts-check
 import { ActivityBlock } from '../../../abstract/ActivityBlock.js';
+import { SolutionBlock } from '../../../abstract/SolutionBlock.js';
 
 export class FileUploaderInline extends SolutionBlock {
+  constructor() {
+    super();
+
+    this.init$ = {
+      ...this.init$,
+      couldBackHistory: false,
+    };
+  }
   shadowReadyCallback() {
     /** @type {import('../../../abstract/UploaderBlock.js').UploaderBlock} */
     const uBlock = this.ref.uBlock;
@@ -13,18 +22,26 @@ export class FileUploaderInline extends SolutionBlock {
 
     this.sub('*uploadList', (list) => {
       if (
-        (list?.length > 0 && this.$['*currentActivity'] === uBlock.initActivity) ||
-        ActivityBlock.activities.START_FROM
+        list?.length > 0 &&
+        this.$['*currentActivity'] === (uBlock.initActivity || ActivityBlock.activities.START_FROM)
       ) {
         this.$['*currentActivity'] = ActivityBlock.activities.UPLOAD_LIST;
       }
     });
+
+    this.sub('*history', (history) => (this.$['couldBackHistory'] = history.length > 1));
   }
 }
 
 FileUploaderInline.template = /* HTML */ ` <lr-start-from>
     <lr-drop-area with-icon clickable></lr-drop-area>
     <lr-source-list wrap></lr-source-list>
+    <button
+      type="button"
+      l10n="cancel"
+      class="cancel-btn secondary-btn"
+      set="onclick: *historyBack; @hidden: !couldBackHistory"
+    ></button>
     <lr-copyright></lr-copyright>
   </lr-start-from>
   <lr-upload-list ref="uBlock"></lr-upload-list>
