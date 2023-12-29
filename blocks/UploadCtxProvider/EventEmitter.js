e0fa212f03c8ac9c1a34c4e994835a9be47658e0
nd0ut <nd0ut.me@gmail.com>
2023-12-29 16:12:46 +0800

    refactor: use spread instead of structuredClone

--- blocks/UploadCtxProvider/EventEmitter.js
+++ blocks/UploadCtxProvider/EventEmitter.js
@@ -98,8 +98,8 @@ export class EventEmitter {
 
     /** @type {import('../../abstract/Block.js').Block | undefined} */
     const firstTarget = this._targets.values().next().value;
-    // We need to use structuredClone here to evaluate all lazy values at the moment of logging
-    firstTarget?.debugPrint(() => [`event "${type}"`, structuredClone(payload)]);
+    // We need to use spread here to evaluate all lazy values at the moment of logging
+    firstTarget?.debugPrint(() => [`event "${type}"`, { ...payload }]);
   }
 
   /**
