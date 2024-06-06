8b5432163b80efc6c99ffe8850fe422f01c75af5
nd0ut <nd0ut@users.noreply.github.com>
2024-06-06 16:53:34 +0300

    Refactor CloudImageEditorSolution solution

--- solutions/cloud-image-editor/CloudImageEditor.js
+++ solutions/cloud-image-editor/CloudImageEditor.js
@@ -1,23 +1,4 @@
 // @ts-check
 import { CloudImageEditorBlock } from '../../blocks/CloudImageEditor/src/CloudImageEditorBlock.js';
 
-export class CloudImageEditor extends CloudImageEditorBlock {
-  initCallback() {
-    super.initCallback();
-
-    /** @private */
-    this.__shadowReady = true;
-
-    this.$['*faderEl'] = this.ref['fader-el'];
-    this.$['*cropperEl'] = this.ref['cropper-el'];
-    this.$['*imgContainerEl'] = this.ref['img-container-el'];
-
-    this.initEditor();
-  }
-
-  async initEditor() {
-    if (this.__shadowReady) {
-      await super.initEditor();
-    }
-  }
-}
+export class CloudImageEditor extends CloudImageEditorBlock {}
