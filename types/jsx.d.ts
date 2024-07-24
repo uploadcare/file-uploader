77fe52f8e48ba04072a19de2b6e6f50034e802f1
Egor Didenko <egor.didenko@list.ru>
2024-07-24 10:25:45 -0400

    feat(types): added JSX types for lr-

--- types/jsx.d.ts
+++ types/jsx.d.ts
@@ -28,7 +28,7 @@ declare namespace JSX {
     'uc-editor-scroller': any;
     'uc-editor-slider': any;
     'uc-editor-toolbar': any;
-    'uc-uc-btn-ui': any;
+    'uc-btn-ui': any;
     'uc-line-loader-ui': any;
     'uc-presence-toggle': any;
     'uc-slider-ui': any;
@@ -59,5 +59,46 @@ declare namespace JSX {
     'uc-file-uploader-inline': CustomElement<FileUploaderInline, CtxAttributes>;
     'uc-upload-ctx-provider': CustomElement<InstanceType<UploadCtxProvider>, CtxAttributes>;
     'uc-config': CustomElement<InstanceType<Config>, CtxAttributes & Partial<ConfigPlainType>>;
+
+    'lr-crop-frame': any;
+    'lr-editor-crop-button-control': any;
+    'lr-editor-filter-control': any;
+    'lr-editor-operation-control': any;
+    'lr-editor-image-cropper': any;
+    'lr-editor-image-fader': any;
+    'lr-editor-scroller': any;
+    'lr-editor-slider': any;
+    'lr-editor-toolbar': any;
+    'lr-lr-btn-ui': any;
+    'lr-line-loader-ui': any;
+    'lr-presence-toggle': any;
+    'lr-slider-ui': any;
+    'lr-icon': any;
+    'lr-img': any;
+    'lr-simple-btn': any;
+    'lr-start-from': any;
+    'lr-drop-area': any;
+    'lr-source-btn': any;
+    'lr-source-list': any;
+    'lr-file-item': any;
+    'lr-modal': any;
+    'lr-upload-list': any;
+    'lr-url-source': any;
+    'lr-camera-source': any;
+    'lr-progress-bar-common': any;
+    'lr-progress-bar': any;
+    'lr-external-source': any;
+    'lr-cloud-image-editor-activity': any;
+    'lr-cloud-image-editor-block': CustomElement<
+      CloudImageEditorBlock,
+      CtxAttributes & ({ uuid: string } | { 'cdn-url': string }) & Partial<{ tabs: string; 'crop-preset': string }>
+    >;
+    'lr-cloud-image-editor': CustomElement<CloudImageEditorBlock, JSX.IntrinsicElements['lr-cloud-image-editor-block']>;
+    'lr-form-input': CustomElement<FormInput, CtxAttributes>;
+    'lr-file-uploader-regular': CustomElement<FileUploaderRegular, CtxAttributes>;
+    'lr-file-uploader-minimal': CustomElement<FileUploaderMinimal, CtxAttributes>;
+    'lr-file-uploader-inline': CustomElement<FileUploaderInline, CtxAttributes>;
+    'lr-upload-ctx-provider': CustomElement<InstanceType<UploadCtxProvider>, CtxAttributes>;
+    'lr-config': CustomElement<InstanceType<Config>, CtxAttributes & Partial<ConfigPlainType>>;
   }
 }
