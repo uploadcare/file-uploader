7ad4d95ec1aa7432f0340061e733da0275cb08c6
nd0ut <nd0ut@users.noreply.github.com>
2025-04-04 03:54:57 +0300

    chore: use vite for dev server

--- types/test/uc-cloud-image-editor.test-d.tsx
+++ types/test/uc-cloud-image-editor.test-d.tsx
@@ -7,6 +7,7 @@
 // @ts-expect-error - no css-src
 () => <uc-cloud-image-editor css-src="url" />;
 
+// TODO: css-src should throw an error, something wrong with the tests or types
 () => <uc-cloud-image-editor ctx-name="my-editor" css-src="url" uuid="123124" />;
 () => <uc-cloud-image-editor ctx-name="my-editor" css-src="url" uuid="123124" tabs="tab" crop-preset="preset" />;
 () => <uc-cloud-image-editor ctx-name="my-editor" css-src="url" cdn-url="url" />;
