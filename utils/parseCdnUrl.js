7cac4c3b7cd2b1e8339c7fe69f7e460de7064fb2
Aleksandr Grenishin <nd0ut.me@gmail.com>
2025-08-21 10:13:33 +0300

    fix(api): allow to use both configured cdnBase and ucarecdn.com as host when calling `addFileFromCdnUrl` (#844)
    Co-authored-by: nd0ut <nd0ut@users.noreply.github.com>

--- utils/parseCdnUrl.js
+++ utils/parseCdnUrl.js
@@ -1,12 +1,15 @@
+import { DEFAULT_CDN_CNAME } from '../blocks/Config/initialConfig.js';
+
 const uuidRegex = /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/i;
 const cdnUrlRegex = new RegExp(`^/?(${uuidRegex.source})(?:/(-/(?:[^/]+/)+)?([^/]*))?$`, 'i');
 
 /** @param {{ url: string; cdnBase: string }} options */
 export const parseCdnUrl = ({ url, cdnBase }) => {
   const cdnBaseUrlObj = new URL(cdnBase);
+  const fallbackCdnBaseUrlObj = new URL(DEFAULT_CDN_CNAME);
   const urlObj = new URL(url);
 
-  if (cdnBaseUrlObj.host !== urlObj.host) {
+  if (cdnBaseUrlObj.host !== urlObj.host && fallbackCdnBaseUrlObj.host !== urlObj.host) {
     return null;
   }
 
