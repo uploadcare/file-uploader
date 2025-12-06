7d8959c88f60bbe76ec6c62a807d382df4b7cdad
nd0ut <nd0ut@users.noreply.github.com>
2025-12-06 20:42:53 +0300

    chore: reorganize root exports

--- src/index.ts
+++ src/index.ts
@@ -11,33 +11,35 @@ export { defineLocale } from './abstract/localeRegistry';
 export { ModalEvents, type ModalId } from './abstract/managers/ModalManager';
 export { toKebabCase } from './utils/toKebabCase';
 
-// Blocks:
+// Shared:
 export { Icon } from './blocks/Icon/Icon';
 export { Img } from './blocks/Img/Img';
 export { Modal } from './blocks/Modal/Modal';
+export { FormInput } from './blocks/FormInput/FormInput';
+export { Copyright } from './blocks/Copyright/Copyright';
 export { ProgressBar } from './blocks/ProgressBar/ProgressBar';
 export { ProgressBarCommon } from './blocks/ProgressBarCommon/ProgressBarCommon';
 export { Select } from './blocks/Select/Select';
-export { SimpleBtn } from './blocks/SimpleBtn/SimpleBtn';
 export { SourceBtn } from './blocks/SourceBtn/SourceBtn';
 export { SourceList } from './blocks/SourceList/SourceList';
 export { Spinner } from './blocks/Spinner/Spinner';
-export { StartFrom } from './blocks/StartFrom/StartFrom';
 export { Thumb } from './blocks/Thumb/Thumb';
+export { ActivityHeader } from './blocks/ActivityHeader/ActivityHeader';
+
+// Composed:
+export { StartFrom } from './blocks/StartFrom/StartFrom';
 export { UploadCtxProvider } from './blocks/UploadCtxProvider/UploadCtxProvider';
 export { UploadList } from './blocks/UploadList/UploadList';
 export { UrlSource } from './blocks/UrlSource/UrlSource';
-export { ActivityHeader } from './blocks/ActivityHeader/ActivityHeader';
 export { CameraSource } from './blocks/CameraSource/CameraSource';
 export * from './blocks/CloudImageEditor/index';
 export { CloudImageEditorActivity } from './blocks/CloudImageEditorActivity/CloudImageEditorActivity';
 export { Config } from './blocks/Config/Config';
-export { Copyright } from './blocks/Copyright/Copyright';
 export { DropArea } from './blocks/DropArea/DropArea';
 export { ExternalSource } from './blocks/ExternalSource/ExternalSource';
 export { FileItem } from './blocks/FileItem/FileItem';
-export { FormInput } from './blocks/FormInput/FormInput';
 export { ExternalUploadSource, UploadSource } from './utils/UploadSource';
+export { SimpleBtn } from './blocks/SimpleBtn/SimpleBtn';
 
 // Abstract:
 export { LitBlock as Block } from './lit/LitBlock';
