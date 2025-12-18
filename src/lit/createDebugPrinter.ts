de0028f9f2a2e22cb1fd6c495cbe022d8283afea
nd0ut <nd0ut@users.noreply.github.com>
2025-12-19 01:50:25 +0300

    fix: prevent validation after context destroy

--- src/lit/createDebugPrinter.ts
+++ src/lit/createDebugPrinter.ts
@@ -1,10 +1,11 @@
+import { sharedConfigKey } from '../abstract/sharedConfigKey';
 import type { PubSub } from './PubSubCompat';
 import type { SharedState } from './SharedState';
 
 export const createDebugPrinter = (getCtx: () => PubSub<SharedState>, scope?: string) => {
   return (...args: unknown[]) => {
     const ctx = getCtx();
-    if (!ctx.read('*cfg/debug')) {
+    if (!ctx.read(sharedConfigKey('debug'))) {
       return;
     }
     let consoleArgs = args;
