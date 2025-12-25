7e808bd071e31577056ea8843ecfd8274ac83b1f
Egor Didenko <egor.didenko.work@gmail.com>
2025-12-25 11:02:38 -0500

    fix(cloud-image-editor): adjusted activityParams (#906)
    * fix(cloud-image-editor): adjusted activityParams
    
    * test: added cloud-image-edit activity with params
    
    * types: updated type for param in the setCurrentActivity method

--- src/blocks/CloudImageEditorActivity/CloudImageEditorActivity.ts
+++ src/blocks/CloudImageEditorActivity/CloudImageEditorActivity.ts
@@ -17,6 +17,7 @@ export class CloudImageEditorActivity extends UploaderBlock {
 
   override get activityParams(): ActivityParams {
     const params = super.activityParams;
+
     if ('internalId' in params) {
       return params;
     }
